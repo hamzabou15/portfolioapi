d558c92cf056f89e040bf33dd8a7bc1e2b23f951
hamzabou <hamza8pro@gmail.com>
2024-02-07 16:27:22 +0100

    fix port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 /////////////////////////////////////////
 const projectRoute = require("./routes/projects");
-const listRoute = require ("./routes/lists");
+const listRoute = require("./routes/lists");
 const emailRoute = require("./routes/emails");
 const bookRoute = require("./routes/books");
 const quoteRoute = require("./routes/quotes");
@@ -15,18 +15,18 @@ dotenv.config();
 
 /// la conexion de mongoss avec espress
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(() =>  console.log("DB Connection Successfull")).catch((err) => console.log(err))
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("DB Connection Successfull")).catch((err) => console.log(err))
 
 // le run du serveur au niveau du port 8080
-app.listen(8080  , () => {
-    console.log("backend is running")
+app.listen(process.env.PORT || 8080, () => {
+  console.log("backend is running")
 })
 
 app.use(express.json()); /// pour le envoi
-app.use("/api/projects" , projectRoute)
-app.use("/api/lists" , listRoute)
-app.use("/api/emails" , emailRoute);
-app.use("/api/books" , bookRoute);
-app.use("/api/quotes" , quoteRoute);
+app.use("/api/projects", projectRoute)
+app.use("/api/lists", listRoute)
+app.use("/api/emails", emailRoute);
+app.use("/api/books", bookRoute);
+app.use("/api/quotes", quoteRoute);
